16072243af06fd70f78c7f7e3cf330ea447c5705
Amber Williams <awilliams1234@live.com>
2020-03-07 21:08:52 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,5 +2,12 @@ class Artist
   
   attr_accessor :name 
   
+<<<<<<< HEAD
 end 
   
+=======
+  def initialize(name)
+    @name = name 
+  end  
+end 
+>>>>>>> 3802b785cc6c1f7533d21edd9e80def231c7186d
